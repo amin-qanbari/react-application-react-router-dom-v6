26221750885c3e2c098e24cf1aa605b6d8504bb0
amin <aminqanbarii1992@gmail.com>
2022-06-13 02:13:43 -0700

    create pricing component and styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './HmeStyles.css'
 import Navbar from '../Navbar/Navbar'
 import Hero from '../Hero/Hero'
+import Pricing from '../Pricing/Pricing'
 
 const Home = () => {
   return (
     <div>
        <Navbar/>
        <Hero/>
+       <Pricing/>
     </div>
   )
 }
